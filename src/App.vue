e8ebb5138ca6a3740b27b2b8910a2dfa89b38861
Carissa Niederer <cniederer@Carissas-MacBook-Pro-2.local>
2018-08-28 21:55:46 -0500

    Add simple styling for today

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>My Calendar</h1>
+    <calendar-view
+      :show-date="showDate"
+      class="">
+      <calendar-view-header
+        slot="header"
+        slot-scope="t"
+        :header-props="t.headerProps"
+        @input="setShowDate" />
+    </calendar-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+  import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'app',
+    data: function() {
+      return { showDate: new Date() }
+    },
+    components: {
+      CalendarView,
+      CalendarViewHeader,
+    },
+    methods: {
+      setShowDate(d) {
+        this.showDate = d;
+      },
+    }
+  }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    height: 67vh;
+    width: 90vw;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .today {
+    .cv-day-number {
+      font-weight: bold;
+    }
+  }
 </style>
