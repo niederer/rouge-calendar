893d78362e51935f6670c1b59c0d41220a6c28d0
Carissa Niederer <cniederer@gmail.com>
2018-09-09 21:02:43 -0500

    get authentication working

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,46 @@
 <template>
   <div id="app">
+    <a href="#" @click.prevent="login" v-if="!activeUser">Log in</a>
+    <a href="#" @click.prevent="logout" v-else>Log out</a>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      activeUser: null
+    };
+  },
+
+  async created() {
+    await this.refreshActiveUser();
+  },
+
+  watch: {
+    // everytime a route is changed refresh the activeUser
+    '$route': 'refreshActiveUser'
+  },
+
+  methods: {
+    login() {
+      this.$auth.loginRedirect();
+    },
+
+    async refreshActiveUser() {
+      this.activeUser = await this.$auth.getUser();
+    },
+
+    async logout() {
+      await this.$auth.logout();
+      await this.refreshActiveUser();
+      this.$router.push('/');
+    }
+  }
+};
+</script>
 
 <style lang="scss">
   #app {
@@ -12,11 +50,5 @@
     width: 90vw;
     margin-left: auto;
     margin-right: auto;
-  }
-
-  .today {
-    .cv-day-number {
-      font-weight: bold;
-    }
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld.vue';
 import CalendarView from '@/components/CalendarView.vue';
+import Auth from '@okta/okta-vue';
 
 Vue.use(Router);
 
@@ -14,11 +15,20 @@ const router = new Router({
       component: HelloWorld,
     },
     {
+      path: '/implicit/callback',
+      component: Auth.handleCallback(),
+    },
+    {
       path: '/calendar',
       name: 'CalendarView',
       component: CalendarView,
+      meta: {
+        requiresAuth: true,
+      },
     },
   ],
 });
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard());
 
 export default router;
